5f23c3176acffd1321f21d4cc77923075ee7f490
Abhishek-bramhawale <sanjivkumarbramhawale4992@gmail.com>
2025-07-18 21:09:30 +0530

    added sites

--- public/contentScript.js
+++ public/contentScript.js
@@ -1,21 +1,36 @@
-const BLOCKED_SITES = [
+const BLOCKED_SITES =[
   "youtube.com",
-  "facebook.com"
-  
+  "medium.com",
+  "quora.com",
+  "reddit.com",
+  "mxplayer.in",
+  "zee5.com",
+  "jiocinema.com",
+  "voot.com",
+  "sonyliv.com",
+  "netflix.com",
+  "primevideo.com",
+  "amazon.com",
+  "hotstar.com",
+  "discord.com",
+  "telegram.org",
+  "facebook.com",
+  "instagram.com",
+  "twitter.com",
+  "x.com",
+  "tiktok.com",
+  "snapchat.com"
 ];
 
 if (BLOCKED_SITES.some(site => window.location.hostname.includes(site))) {
-   
-      document.documentElement.innerHTML = `
-        <body style="background:black;color:white;display:flex;justify-content:center;align-items:center;height:100vh;font-family:sans-serif;">
-          <div style="text-align:center;">
-
-            <h1>Social media detected - </h1>
-            <h2>Blocked</h1>
-            <h3>Go Study Right Now!</h2>
-
-          </div>
-        </body>
-      `;
- 
-  }
+  setInterval(()=>{
+    document.documentElement.innerHTML= `
+      <body style="background:black;color:white;display:flex;justify-content:center;align-items:center;height:100vh;font-family:sans-serif;">
+        <div style="text-align:center;">
+          <h1>Blocked</h1>
+          <h2>Go Study Right Now!</h2>
+        </div>
+      </body>
+    `;
+  },100);
+}
